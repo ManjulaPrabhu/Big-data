6326600f88caea7a7e8da61a0b4538682f0978e2
Manjula Bashini Prabhu <31738763+ManjulaPrabhu@users.noreply.github.com>
2018-12-10 17:26:27 -0800

    Added solution to checkin analysis

---
+++ Final Project/Check In Analysis/src/main/java/assignment/checkinAnalysis/CheckInAnalysisDriver.java
@@ -0,0 +1,29 @@
+package assignment.checkinAnalysis;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class CheckInAnalysisDriver {
+	public static void main(String[] args) throws Exception{
+		Job job=Job.getInstance();
+		job.setJarByClass(CheckInAnalysisDriver.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setMapperClass(CheckInAnalysisMapper.class);
+		job.setReducerClass(CheckInAnalysisReducer.class);
+		
+		job.setNumReduceTasks(2);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.waitForCompletion(true);
+		}
+}
